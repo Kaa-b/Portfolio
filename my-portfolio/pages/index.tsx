70ff869994dfbe2896a31b6f4fbeba5fa4ab7603
Marine <101167755+Kaa-b@users.noreply.github.com>
2023-03-19 16:17:24 +0000

    add basic styling for  home page

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -4,8 +4,7 @@ import Image from "next/image"
 import Navbar from "../components/nav/navbar"
 import Footer from "../components/footer/footer"
 
-import styles from "@/styles/Home.module.css"
-
+import styles from "@/styles/home.module.css"
 
 
 export default function Home() {
@@ -17,11 +16,20 @@ export default function Home() {
         {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <Navbar />
-      <main>
-      Home page
-      </main>
-      <Footer/>
+      <div className={styles.body}>
+        <nav className={styles.nav}>
+          <Navbar />
+        </nav>
+        <main className={styles.container}>
+          <h1 className={styles.description}>
+          Preparing <b>Home</b> page for take-off...
+          </h1>
+        
+        </main>
+        <footer className={styles.footer}>
+          <Footer/>
+        </footer>
+      </div>
     </>
   )
 }
