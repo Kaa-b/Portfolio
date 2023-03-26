de3713669f77adcbde229ba590cbbb4b7e725906
Marine <101167755+Kaa-b@users.noreply.github.com>
2023-03-26 18:23:56 +0000

    finish header and startedlast project

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
+import cls from "classnames"
 
 import Navbar from "../components/nav/navbar"
 import Footer from "../components/footer/footer"
@@ -17,60 +18,81 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <div className={styles.body}>
-        <nav className={styles.nav}>
-          <Navbar />
-        </nav>
+        <Navbar />
         <main className={styles.container}>
           <div className={styles.header}>
             <div className={styles.heroImage}>
-              <div className={styles.profilePicture}>
-                <Image
-                  src="/profilePicture.svg"
-                  alt="My profile picture"
-                  width="259"
-                  height="378"
-                />
-              </div>
+              <Image
+                src="/profilePicture.svg"
+                alt="My profile picture"
+                width="259"
+                height="378"
+              />
               <div className={styles.socials}>
-                <a className={styles.github} href="https://github.com/Kaa-b" >
+                <a className={styles.socialItem} href="https://github.com/Kaa-b" >
                   <Image
                     src="/github.svg"
                     alt="Redirect to my github page"
-                    width="48"
-                    height="48"
+                    width="40"
+                    height="40"
                   />
                 </a>
-                <a className={styles.linkedIn} href="https://www.linkedin.com/in/marine-bauer/">
+                <a className={styles.socialItem} href="https://www.linkedin.com/in/marine-bauer/">
                   <Image
                     src="/linkedIn.svg"
                     alt="Redirect to my linkedIn page"
-                    width="48"
-                    height="48"
+                    width="40"
+                    height="40"
                   />
                 </a>
               </div>
             </div>
-            <div className={styles.description}></div>
+            <div className={styles.introduction}>
+              <h1 className={styles.title}>Welcome to my Website!</h1>
+              <p className={styles.description}>
+                Porem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit.
+              </p>
+              <p className={styles.description}>Keep scrolling to find out about my latest project</p>
+              <div className={styles.redirection}>
+                <div className={styles.redirectionOption}>
+                  <p className={styles.description}>Pour voir mon parcours de formation en dev c’est par là !</p>
+                  <button className={cls(styles.arrow, styles.down)}></button>
+                </div>
+                <div className={styles.redirectionOption}>
+                  <p className={styles.description}>Pour voir ma présentation c’est ici !</p>
+                  <button className={cls(styles.arrow, styles.right)}></button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.latestProject}>
-            <h1 className={styles.description}>
-              Latest project
-            </h1>
+            <h1 className={styles.title}>Latest project</h1>
+            <div className={styles.latestProjectContent}>
+              <div className={styles.latestProjectdescription}>
+                <p className={styles.description}>Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+              </div>
+              <a className={styles.screen} href="https://street-contact.vercel.app/">
+                <Image
+                  src="/screen.svg"
+                  alt="My latest project"
+                  width="430"
+                  height="346"
+                />
+              </a>
+            </div>
           </div>
           <div className={styles.devEvolution}>
-            <h1 className={styles.description}>
+            <h1 className={styles.title}>
               Dev Evolution
             </h1>
           </div>
           <div className={styles.contact}>
-            <h1 className={styles.description}>
+            <h1 className={styles.title}>
               Contact
             </h1>
           </div>       
         </main>
-        <footer className={styles.footer}>
-          <Footer/>
-        </footer>
+        <Footer/>
       </div>
     </>
   )
